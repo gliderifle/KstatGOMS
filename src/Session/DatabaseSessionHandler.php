4fdac9e6c571528dacbb75e67e965d19f71f64fc
SJ Kim <gliderifle@gmail.com>
2020-09-22 17:14:21 +0900

    Update DatabaseSessionHandler.php

--- src/Session/DatabaseSessionHandler.php
+++ src/Session/DatabaseSessionHandler.php
@@ -18,29 +18,29 @@ class DatabaseSessionHandler implements \SessionHandlerInterface
 
   public function read($id)
   {
-    $data = current([Adaptor::getAll('SELECT * FROM kstatcontact.sessions WHERE id=?', [$id])]);
+    $data = current([Adaptor::getAll('SELECT * FROM sessions WHERE id=?', [$id])]);
 
     if($data){      
       $payload = $data[0]->payload;
     } else {
-      Adaptor::exec('INSERT INTO kstatcontact.sessions(`id`) VALUES(?)',[$id]);
+      Adaptor::exec('INSERT INTO sessions(`id`) VALUES(?)',[$id]);
     }
     return $payload ?? '';
   }
 
   public function write($id, $payload)
   {
-    return Adaptor::exec('UPDATE kstatcontact.sessions SET payload = ? WHERE id=?',[$payload, $id]);
+    return Adaptor::exec('UPDATE sessions SET payload = ? WHERE id=?',[$payload, $id]);
   }
 
   public function destroy($id)
   {
-    return Adaptor::exec('DELETE FROM kstatcontact.sessions WHERE id=?',[$id]);
+    return Adaptor::exec('DELETE FROM sessions WHERE id=?',[$id]);
   }
 
   public function gc($maxlifetime)
   {
-    if($sessions = Adaptor::getAll('SELECT * FROM kstatcontact.sessions')){
+    if($sessions = Adaptor::getAll('SELECT * FROM sessions')){
       foreach($sessions as $session){
         $timestamp = strtotime($session->created_at);
         if(time() - $timestamp > $maxlifetime){
